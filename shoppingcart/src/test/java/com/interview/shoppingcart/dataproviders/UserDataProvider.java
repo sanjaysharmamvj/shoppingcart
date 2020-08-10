d2a2f0abd7a12dd3dd2d4ee9f957d699c028d544
Sharma,Sanjay <Sanjay.Sharma@Cerner.com>
2020-08-10 19:28:30 +0530

    Pushing changes since JSON added as data provider

--- shoppingcart/src/test/java/com/interview/shoppingcart/dataproviders/UserDataProvider.java
+++ shoppingcart/src/test/java/com/interview/shoppingcart/dataproviders/UserDataProvider.java
@@ -1,55 +1,33 @@
 package com.interview.shoppingcart.dataproviders;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
+import java.util.InputMismatchException;
 import java.util.Map;
 import java.util.Random;
 
-
 import org.testng.annotations.DataProvider;
 
-import com.google.common.reflect.TypeToken;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class UserDataProvider {
-	
-	@DataProvider(name="userRegistrationdata")
-	public Object[][] userRegistrationData() {
-		Map<String, String> userData = new HashMap<String,String>();
-	    Random random = new Random();
-	    String emailId = "sanjaytest" + random.nextInt(5) + "@abc.com";  	
-		userData.put("email", emailId);
-		userData.put("firstName", "Sanjay");
-		userData.put("lastName", "Sharma");
-		userData.put("password", "Welcome123");
-		userData.put("fName", "Sanjay");
-		userData.put("lName", "Sharma");
-		userData.put("address", "Cerner,12 LakeTown,Block-B");
-		userData.put("city", "Philadelphia");
-		userData.put("state", "Pennslyvania");
-		userData.put("pincode", "15001");
-		userData.put("country", "United States");
-		userData.put("mobileNo", "90515567");
-		userData.put("aliasAddress", "Res");
-		Object[][]testData={{userData}};
-		return testData;		
-		
+
+	@DataProvider(name = "userRegistrationdata")
+	public Object[][] getRegistrationData() throws FileNotFoundException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		Map<String, String> jsonData = new HashMap<String, String>();
+		try {
+			FileInputStream fileInput = new FileInputStream("C://Users/ss041118/git/ShoppingCart/shoppingcart/src/test/resources/testdata/user_data.json");
+			jsonData = objectMapper.readValue(fileInput, Map.class);
+		} catch (FileNotFoundException fnfe) {
+			fnfe.printStackTrace();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		} 
+		return new Object[][] { { jsonData } };
+
 	}
-	/*
-	@DataProvider
-    public Object[][] userRegistrationData1() throws FileNotFoundException {
-        JsonElement jsonData = new JsonParser().parse(new FileReader("src/test/resources/45146523.json"));
-        JsonElement dataSet = jsonData.getAsJsonObject().get("dataSet");
-        List<TestData> testData = new Gson().fromJson(dataSet, new TypeToken<List<TestData>>() {
-        }.getType());
-        Object[][] returnValue = new Object[testData.size()][1];
-        int index = 0;
-        for (Object[] each : returnValue) {
-            each[0] = testData.get(index++);
-        }
-        return returnValue;
-    }
-}*/
 
 }
--- shoppingcart/src/test/java/com/interview/shoppingcart/tests/CreateAccountTest.java
+++ shoppingcart/src/test/java/com/interview/shoppingcart/tests/CreateAccountTest.java
@@ -9,6 +9,7 @@ import org.testng.annotations.Test;
 
 import com.interview.shoppingcart.common.BrowserFactory;
 import com.interview.shoppingcart.common.CommonTask;
+import com.interview.shoppingcart.dataproviders.UserDataProvider;
 import com.interview.shoppingcart.pages.AuthenticationPage;
 import com.interview.shoppingcart.pages.CreateAnAccountPage;
 import com.interview.shoppingcart.pages.HomePage;
@@ -25,16 +26,18 @@ public class CreateAccountTest {
 
 	}
 
-	@Test(dataProvider="userRegistrationdata")
+	@Test(dataProvider="userRegistrationdata", dataProviderClass=UserDataProvider.class)
 	public void createAccount(Map<Object, Object> map) {
 		HomePage homePage = new HomePage(driver);		
 		homePage.clickSignIn();
 		AuthenticationPage authentication = new AuthenticationPage(driver);
-		authentication.createAnAccount("sanjay12@abc.com");
+		authentication.createAnAccount("sanjay122@abc.com");
 		CreateAnAccountPage createAccount = new CreateAnAccountPage(driver);
 		createAccount.enterPersonalInfo("sanjay@abc.com","Sanjay","Sharma","Welcome123","Sanjay",
 				"Sharma","Cerner,12 LakeTown,Block-B","Philadelphia","Pennslyvania","15001","United States","90515567","Res");
 		createAccount.registerAccount();
+		
+		System.out.println("Data from the DataProvider" +map);
 	}
 
 	@AfterTest
